42418d4759a9e8694a61e5a7c0a994fe0c5f1595
Toleco <baptiste.blst@gmail.com>
2023-07-30 00:46:13 +0200

    card # ajout card / grid(home) / randompokemon(home)

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,11 @@
 
 <template>
   <div>
-    <Card
-        v-for="pokemon in pokedex" :key="pokemon.id"
+    <br><br><br>
+    <!-- <div class="container">
+      <Card
+        v-for="pokemon in pokedex"
+        :key="pokemon.id"
         :imageLink="`/images/${
           ('00' + pokemon.id).slice(-3) + pokemon.name.english + '.png'
         }`"
@@ -13,7 +16,8 @@
         :type1="pokemon.type[0]"
         :type2="pokemon.type[1]"
         :pokemonLink="pokemon.id - 1"
-      />
+        />
+    </div> -->
     <!-- <div v-for="pokemon in pokedex" :key="pokemon.id">
       <h2>{{ pokemon.name.english }}</h2>
       <p>Types: {{ pokemon.type.join(', ') }}</p>
@@ -24,6 +28,27 @@
       <p>Sp. Defense: {{ pokemon.base['Sp. Defense'] }}</p>
       <p>Speed: {{ pokemon.base.Speed }}</p>
     </div> -->
+    {{ randomPokemon }}
+    <div class="container">
+      <div class="pokedex">
+        <div class="pokedex-aside">fds</div>
+        <div class="pokedex-list">
+          <Card
+            v-for="pokemon in pokedex"
+            :key="pokemon.id"
+            :imageLink="`/images/${
+              ('00' + pokemon.id).slice(-3) + pokemon.name.english + '.png'
+            }`"
+            :id="pokemon.num"
+            :name="pokemon.name.english"
+            :newClass="pokemon.type[0].toLowerCase()"
+            :type1="pokemon.type[0]"
+            :type2="pokemon.type[1]"
+            :pokemonLink="pokemon.id - 1"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,15 +62,57 @@ export default {
   computed: {
     pokedex() {
       return this.$store.state.pokedex;
-    }
+    },
+    randomPokemon(){
+      if (this.pokedex.length === 0) return null;
+      const randomIndex = Math.floor(Math.random() * this.pokedex.length);
+      return this.pokedex[randomIndex];    }
   },
   async mounted() {
-    await this.$store.dispatch('fetchPokedex');
-  }
+    await this.$store.dispatch("fetchPokedex");
+  },
 };
 </script>
 
 <style lang="scss">
+.pokedex {
+  background: red;
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  @media screen and (min-width: 1200px) {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-template-rows: 1fr;
+    grid-column-gap: 10px;
+    grid-row-gap: 10px;
+  }
+
+  &-aside {
+    background: bisque;
+    @media screen and (min-width: 1200px) {
+      grid-area: 1 / 1 / 2 / 2;
+    }
+  }
+  &-list {
+    background: rebeccapurple;
+    display: grid;
+    grid-auto-rows: minmax(100px, auto);
+    grid-template-columns: repeat(1, 1fr);
+    @media screen and (min-width: 576px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+    @media screen and (min-width: 768px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+    @media screen and (min-width: 992px) {
+      grid-template-columns: repeat(3, 1fr);
+    }
+    @media screen and (min-width: 1200px) {
+      grid-area: 1 / 2 / 2 / 5;
+    }
+  }
+}
 .container {
   width: 100%;
   margin-right: auto;
@@ -55,121 +122,14 @@ export default {
     max-width: 540px;
   }
   @media screen and (min-width: 768px) {
-    max-width: 720px;
+    max-width: 750px;
   }
   @media screen and (min-width: 992px) {
-    max-width: 960px;
+    max-width: 970px;
   }
   @media screen and (min-width: 1200px) {
-    max-width: 1140px;
+    max-width: 1170px;
   }
 }
-.pokedex {
-  display: grid;
-  padding-inline-start: 0;
-  height: 80vh;
-  overflow: hidden;
-  overflow-y: scroll;
-  scrollbar-color: #ab1515 var(--water-secondary);
-  scrollbar-width: thin;
-  grid-template-columns: 1fr;
-  grid-template-rows: 1fr 1fr 1fr 1fr;
-  width: 100%;
-  @media screen and (min-width: 576px) {
-    grid-template-columns: 1fr 1fr;
 
-  }
-
-  @media screen and (min-width: 992px) {
-    grid-template-columns: 1fr 1fr 1fr;
-
-  }
-  @media screen and (min-width: 1200px) {
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-
-  }
-  &-group{
-    display: flex;
-    flex-direction: row;
-  }
-  &-search{
-    background: #fff;
-    padding: 0 20px;
-    transition: .3s;
-    border: 1px solid #ab1515;
-    border-radius: 4px;
-    display: block;
-    font-size: 12px;
-    font-weight: 400;
-    height: 32px;
-    position: absolute;
-    right: 12px;
-    top: 30%;
-    outline:  none;
-    &:focus{
-      box-shadow: inset 0 0 0px 2px #c42829;
-    }
-    @media screen and (max-width: 768px) {
-      top: 50%;
-      right: 30px;
-    }
-  }
-  &-completed {
-    display: flex;
-    flex-direction: column;
-    border-radius: 50px 0 0 0;
-    background-color: #c63030;
-    margin-top: 25px;
-    border-radius: 30px 10px 10px 5px;
-  }
-  &-lence {
-    width: calc(100% - 15px);
-    height: 60px;
-    background-color: #c42829;
-    padding: 15px 0 0 15px;
-    top: 0;
-    left: 0;
-    box-shadow: 0px 6px #ab1515;
-    border-radius: 30px 10px 30px 0px;
-    margin-bottom: 25px;
-    position: relative;
-  }
-  &-glass {
-    width: 40px;
-    height: 40px;
-    margin-right: 15px;
-    border: solid 4px white;
-    background: radial-gradient(var(--water), var(--water-secondary));
-    border-radius: 35px;
-  }
-  &-reflect {
-    width: 15px;
-    height: 9px;
-    margin: 5px 0 0 11px;
-    background: white;
-    opacity: 0.6;
-    border-radius: 15px 9px;
-  }
-  &-first {
-    width: 18px;
-    height: 18px;
-    margin-right: 9px;
-    border-radius: 20px;
-    background: radial-gradient(var(--poison), var(--poison-secondary));
-  }
-  &-second {
-    width: 18px;
-    height: 18px;
-    margin-right: 9px;
-    border-radius: 18px;
-    background: radial-gradient(var(--fire), var(--fire-secondary));
-  }
-  &-third {
-    width: 18px;
-    height: 18px;
-    margin-right: 9px;
-    border-radius: 18px;
-    background: radial-gradient(var(--grass), var(--grass-secondary));
-  }
-}
 </style>
